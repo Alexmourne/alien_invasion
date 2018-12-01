5b33879404d5b4408ba30f94eeb91e93a9caf5ad
Alexmourne <lixumourne@hotmail.com>
2018-12-02 00:20:10 +0800

    week48

--- alien_invasion/alien_invasion/ship.py
+++ alien_invasion/alien_invasion/ship.py
@@ -3,9 +3,10 @@ import pygame
 class Ship(object):
     """description of class"""
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         #
         self.image = pygame.image.load('images/ship.bmp')
@@ -16,7 +17,32 @@ class Ship(object):
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        #return super().__init__(*args, **kwargs)
+        #
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
+
+        #
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            #self.rect.centerx += 1
+            self.centerx += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            #self.rect.centerx -= 1
+            self.centerx -= self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top > 0:
+            self.centery -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.centery += self.ai_settings.ship_speed_factor
+
+        #
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
 
     def blitme(self):
         """"""
